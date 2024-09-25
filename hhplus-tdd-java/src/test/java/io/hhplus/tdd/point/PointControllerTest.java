084e18fbdca468986973955e990d00efd19c56b1
Seongjun Im <80080990+choral7451@users.noreply.github.com>
2024-09-25 22:25:51 +0900

    test: 포인트 컨트롤러 테스트 수정 포인트 컨트롤러 통합테스트를 단위테스트로 수정했습니다.

--- hhplus-tdd-java/src/test/java/io/hhplus/tdd/point/PointControllerTest.java
+++ hhplus-tdd-java/src/test/java/io/hhplus/tdd/point/PointControllerTest.java
@@ -1,20 +1,21 @@
 package io.hhplus.tdd.point;
 
+import static org.mockito.Mockito.*;
 import static org.springframework.http.MediaType.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import java.util.List;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-
-import io.hhplus.tdd.database.PointHistoryTable;
-import io.hhplus.tdd.database.UserPointTable;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -26,29 +27,29 @@ class PointControllerTest {
 	@Autowired
 	private ObjectMapper objectMapper;
 
-	@Autowired
-	private UserPointTable userPointTable;
-
-	@Autowired
-	private PointHistoryTable pointHistoryTable;
+	@MockBean
+	private PointService pointService;
 
 	@Test
 	@DisplayName("특정 사용자의 포인트를 조회합니다.")
 	public void getUserPoint() throws Exception {
-		// given
+		//given
 		long userId = 1L;
 		long point = 1000L;
 
-		userPointTable.insertOrUpdate(userId, point);
+		UserPoint userPoint = new UserPoint(userId, point, System.currentTimeMillis());
+		when(pointService.getUserPoint(userId)).thenReturn(userPoint);
 
 		// when
 		mockMvc.perform(get("/point/{id}", userId)
 				.contentType(APPLICATION_JSON))
 
-		// then
+			// then
 			.andExpect(status().isOk())
-			.andExpect(jsonPath("$.id").value(userId))
-			.andExpect(jsonPath("$.point").value(point));
+			.andExpect(jsonPath("$.id").value(userPoint.id()))
+			.andExpect(jsonPath("$.point").value(userPoint.point()));
+
+		verify(pointService).getUserPoint(anyLong());
 	}
 
 	@Test
@@ -59,8 +60,14 @@ class PointControllerTest {
 		long chargedPoint = 1000L;
 		long usedPoint = 500L;
 
-		pointHistoryTable.insert(userId, chargedPoint, TransactionType.CHARGE, System.currentTimeMillis());
-		pointHistoryTable.insert(userId, usedPoint, TransactionType.USE, System.currentTimeMillis());
+		PointHistory chargePointHistory = new PointHistory(1L, userId, chargedPoint, TransactionType.CHARGE,
+			System.currentTimeMillis());
+
+		PointHistory usePointHistory = new PointHistory(2L, userId, usedPoint, TransactionType.USE,
+			System.currentTimeMillis());
+
+		List<PointHistory> pointHistories = List.of(chargePointHistory, usePointHistory);
+		when(pointService.getPointHistories(userId)).thenReturn(pointHistories);
 
 		// when
 		mockMvc.perform(get("/point/{id}/histories", userId)
@@ -84,7 +91,8 @@ class PointControllerTest {
 		long point = 1000L;
 		long chargePoint = 500L;
 
-		userPointTable.insertOrUpdate(userId, point);
+		UserPoint userPoint = new UserPoint(userId, point + chargePoint, System.currentTimeMillis());
+		when(pointService.charge(userId, chargePoint)).thenReturn(userPoint);
 
 		// when
 		mockMvc.perform(patch("/point/{id}/charge", userId)
@@ -94,8 +102,8 @@ class PointControllerTest {
 
 			// then
 			.andExpect(status().isOk())
-			.andExpect(jsonPath("$.id").value(userId))
-			.andExpect(jsonPath("$.point").value(point + chargePoint));
+			.andExpect(jsonPath("$.id").value(userPoint.id()))
+			.andExpect(jsonPath("$.point").value(userPoint.point()));
 	}
 
 	@Test
@@ -106,7 +114,8 @@ class PointControllerTest {
 		long point = 1000L;
 		long usePoint = 500L;
 
-		userPointTable.insertOrUpdate(userId, point);
+		UserPoint userPoint = new UserPoint(userId, point + usePoint, System.currentTimeMillis());
+		when(pointService.use(userId, usePoint)).thenReturn(userPoint);
 
 		// when
 		mockMvc.perform(patch("/point/{id}/use", userId)
@@ -116,7 +125,7 @@ class PointControllerTest {
 
 			// then
 			.andExpect(status().isOk())
-			.andExpect(jsonPath("$.id").value(userId))
-			.andExpect(jsonPath("$.point").value(point - usePoint));
+			.andExpect(jsonPath("$.id").value(userPoint.id()))
+			.andExpect(jsonPath("$.point").value(userPoint.point()));
 	}
 }
